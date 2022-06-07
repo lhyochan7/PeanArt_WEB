0fee7626195f7a2c6924afbb88ea3ffa3c1c9135
cloudsoswift <77185436+cloudsoswift@users.noreply.github.com>
2022-06-07 18:12:04 +0900

    Update vue.config.js

--- PeanArt_Vue/vue.config.js
+++ PeanArt_Vue/vue.config.js
@@ -1,6 +1,28 @@
 const { defineConfig } = require('@vue/cli-service')
+const path = require("path");
 module.exports = defineConfig({
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  devServer: {
+    proxy: {
+        '/':{
+            "target":'http://localhost:8080', // Spring boot의 주소 및 포트
+            "pathRewrite":{'^/':''},
+            "changeOrigin":true,
+            logLevel: 'debug', // 터미널에 proxy 로그가 찍힌다.
+            ws: true,
+        }
+    }
+  },
+    // Template for index.html
+    // 기존 경로: index: path.resolve(__dirname, '../dist/index.html'),
+    //  SpringBoot Static Folder
+  indexPath: "../templates/index.html",
+  assetsDir: "vuestatic",
+    // Paths
+    // 기존 경로: assetsRoot: path.resolve(__dirname, '../dist'),
+    //  SpringBoot Static Folder
+  outputDir: '../src/main/resources/static',
+  publicPath: '/',
 })
