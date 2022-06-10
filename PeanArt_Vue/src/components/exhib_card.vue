5f1077ca8e39b18c22a2f447f3b0327be034f503
cloudsoswift <magic9282m@gmail.com>
2022-06-10 18:02:54 +0900

    making bone of exhibit views

--- PeanArt_Vue/src/components/exhib_card.vue
+++ PeanArt_Vue/src/components/exhib_card.vue
@@ -1,23 +1,24 @@
 <template>
-<v-col sm="4">
-  <v-card
-    class="rounded-xl mt-4 mb-4"
-    elevation="0"
-    outlined
-  >
-      <v-img
-       src="../assets/ai_1.png"
-       height="200px"
+    <v-col sm="4">
+  <router-link style="text-decoration:none; color:black;" to="id/detail.do">
+      <v-card
+        class="rounded-xl mt-4 mb-4"
+        elevation="0"
+        outlined
       >
-      </v-img>
-      <v-card-title>
-          서울대 전시회
-      </v-card-title>
-      <v-card-subtitle>
-          2022-05-01 ~ 2022-06-01
-      </v-card-subtitle>
-  </v-card>
-</v-col>
+          <v-img
+          src="../assets/ai_1.png"
+          height="200px"
+          >
+          </v-img>
+          <v-card-text class="ml-n4 mb-n2">
+              <v-card-title class="mt-n4 mb-3" style="color:black;">서울대 전시회</v-card-title>
+              <v-card-subtitle>2022-06-01 ~ 2022-07-24</v-card-subtitle>
+              <v-card-text>위치</v-card-text>
+          </v-card-text>
+      </v-card>
+  </router-link>
+    </v-col>
 </template>
 
 <script>
---
+++ PeanArt_Vue/src/pages/exhibitListView.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app>
+      <nav_bar></nav_bar>
+      <v-container>
+          <v-row>
+              <v-col>
+                  <exhib_card></exhib_card>
+              </v-col>
+          </v-row>
+      </v-container>
+  </v-app>
+</template>
+
+<script>
+import exhib_card from '../components/exhib_card.vue'
+import nav_bar from '../components/nav_bar.vue'
+export default {
+    name: 'exhibitListView',
+    components: { nav_bar, exhib_card },
+    data: () => ({
+
+    }),
+
+}
+</script>
+
+<style>
+
+</style>
