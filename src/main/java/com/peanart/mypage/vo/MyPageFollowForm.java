b340b6f73c3e468e0495135ffffb44845d305caf
kids9702 <kamji9702@naver.com>
2022-06-16 14:11:50 +0900

    0615 mypage include url

--- src/main/java/com/peanart/mypage/vo/MyPageFollowForm.java
+++ src/main/java/com/peanart/mypage/vo/MyPageFollowForm.java
@@ -1,50 +1,11 @@
 package com.peanart.mypage.vo;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter@Setter@ToString
 public class MyPageFollowForm {
-   private String fileDirName;
-   private String fileName;
-   private String usrId;
-   private String usrNickname;
-
-    public String getFiledDirName() {
-        return fileDirName;
-    }
-
-    public void setFiledDirName(String filedDirName) {
-        this.fileDirName = fileDirName;
-    }
-
-    public String getFileName() {
-        return fileName;
-    }
-
-    public void setFileName(String fileName) {
-        this.fileName = fileName;
-    }
-
-    public String getUsrId() {
-        return usrId;
-    }
-
-    public void setUsrId(String usrId) {
-        this.usrId = usrId;
-    }
-
-    public String getUsrNickname() {
-        return usrNickname;
-    }
-
-    public void setUsrNickname(String usrNickname) {
-        this.usrNickname = usrNickname;
-    }
-
-    @Override
-    public String toString() {
-        return "MyPageFollowForm{" +
-                "filedDirName='" + fileDirName + '\'' +
-                ", fileName='" + fileName + '\'' +
-                ", usrId='" + usrId + '\'' +
-                ", usrNickname='" + usrNickname + '\'' +
-                '}';
-    }
+    private String usrNickname;
+    private String followedImgUrl;
 }
---
+++ src/main/java/com/peanart/mypage/vo/MyPageFollowVO.java
@@ -0,0 +1,13 @@
+package com.peanart.mypage.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter@Setter@ToString
+public class MyPageFollowVO {
+   private String fileDirName;
+   private String fileName;
+   private String usrId;
+   private String usrNickname;
+}
