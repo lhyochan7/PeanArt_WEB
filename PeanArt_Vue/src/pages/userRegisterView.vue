8229342ae21222dbcb60c7a13da75cc9fb15a91a
kids9702 <hn01184@gmail.com>
2022-06-20 16:02:30 +0900

    포스터 바꾸깅w

--- PeanArt_Vue/src/pages/userRegisterView.vue
+++ PeanArt_Vue/src/pages/userRegisterView.vue
@@ -191,7 +191,7 @@ export default {
               return;
           }
           const param = { params:{email: this.usr_email} }
-          axios.get("http://15.164.142.253:8080/duple-check", param).then(response =>{
+          axios.get("http://localhost:8080/duple-check", param).then(response =>{
               console.log(response);
               if(response.status === 404){
                   alert('에러가 발생했습니다. 잠시후 시도해주세요.');
@@ -216,7 +216,7 @@ export default {
               return;
           }
           const param = { params:{nickname: this.usr_nickname} }
-          axios.get("http://15.164.142.253:8080/duple-check", param).then(response =>{
+          axios.get("http://localhost:8080/duple-check", param).then(response =>{
               console.log(response);
               if(response.status === 404){
                   alert('에러가 발생했습니다. 잠시후 시도해주세요.');
